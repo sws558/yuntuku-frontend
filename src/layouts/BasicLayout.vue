41811c8be1b859713d7ba089d4675d2a4b65982a
sws <1263654270@qq.com>
2025-02-05 21:40:42 +0800

    Initial commit

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+      <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+      <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer" >
+        <a href="https://www.codefather.cn" target="_blank">
+        编程导航bysws
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+<style scoped>
+#basicLayout .header{
+  background: white;
+  color:unset;
+  margin-bottom: 16px;
+  padding-inline: 20px;
+
+}
+
+#basicLayout .content{
+  padding: 20px;
+  background: linear-gradient(0deg, #fff, #efefef);
+  margin-bottom: 28px;
+}
+#basicLayout .footer{
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0 ;
+  right: 0;
+  text-align: center;
+}
+
+</style>
