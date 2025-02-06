35cf5c8d7faa099bfdf27e131b4ae7802a19c84e
sws <1263654270@qq.com>
2025-02-06 13:40:07 +0800

    实现了推出登录功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -20,8 +20,22 @@
       <a-col flex="120px">
         <div class="user-login-status">
           <div v-if="loginUserStore.loginUser.id">
-            {{ loginUserStore.loginUser.userName ?? '无名' }}
+            <a-dropdown>
+              <ASpace>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </ASpace>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
           </div>
+
           <div v-else>
             <a-button type="primary" href="/user/login">登录</a-button>
           </div>
@@ -35,7 +49,9 @@ import { h, ref } from 'vue'
 import { HomeOutlined } from '@ant-design/icons-vue'
 import type { MenuProps } from 'ant-design-vue'
 import { useRouter } from 'vue-router'
-import {useLoginUserStore} from "@/stores/useLoginUserStore.ts"; // 导入 useRouter
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import {userLogoutUsingPost} from "@/api/userController.ts";
+import {message} from "ant-design-vue"; // 导入 useRouter
 const loginUserStore = useLoginUserStore()
 const items = ref<MenuProps['items']>([
   {
@@ -62,6 +78,22 @@ router.afterEach((to, from, next) => {
   current.value = [to.path]
 })
 //路由跳转事件
+
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  console.log(res)
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
+
 const doMenuClick = ({ key }: { key: string }) => {
   router.push({
     path: key,
